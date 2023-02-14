023cf0ea0e39bc6080403073dd719a473ee439df
Carlos Pérez Aguilera <capeag12@gmail.com>
2023-02-14 13:33:22 +0100

    V1

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,13 +2,13 @@ const jwt = require("jsonwebtoken")
 const Usuario = require("../models/Usuario")
 
 const auth = async (req,res,next) => {
-    
     try{
         const token = req.header('Authorization').replace('Bearer ', '')
+        console.log("Token: "+token)
         const decoded = jwt.verify(token, 'nuevousuario')
         
         const usuario = await Usuario.findOne({_id:decoded._id, "tokens.token":token})
-        
+        console.log(usuario)
         if (!usuario) {
             throw new Error()
         }
